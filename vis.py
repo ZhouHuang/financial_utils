1da05d5e851985fe91cc5a68998bf0fe338601e9
Zhou <zhouh17@sjtu.edu.cn>
2022-10-30 16:19:12 +0800

    animator x tick can be shown in datetime unit

--- vis.py
+++ vis.py
@@ -8,7 +8,7 @@ class Animator:
     def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
                  ylim=None, xscale='linear', yscale='linear',
                  fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
-                 figsize=(3.5, 2.5)):
+                 figsize=(3.5, 2.5), date_list=None):
         # 增量地绘制多条线
         if legend is None:
             legend = []
@@ -19,6 +19,7 @@ class Animator:
         self.config_axes = lambda: self.set_axes(
             self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
         self.X, self.Y, self.fmts = None, None, fmts
+        self.date_list = date_list
 
     def set_axes(self, axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
 
@@ -31,6 +32,12 @@ class Animator:
         if legend:
             axes.legend(legend)
         axes.grid()
+        if not self.date_list is None:
+            date_length = len(self.date_list)
+            N_date_ticks = 8
+            plt.xticks(range(0,date_length, int(date_length/N_date_ticks)), 
+                   [str(day)[:10] for day in self.date_list[::int(date_length/N_date_ticks)] ], 
+                   rotation=45)
 
     def add(self, x, y):
         # 向图表中添加多个数据点
@@ -54,19 +61,6 @@ class Animator:
         display.display(self.fig)
         display.clear_output(wait=True)
 
-"""
-def vis_demo(net, train_iter, test_iter, loss, num_epochs, updater):
 
-    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
-                        legend=['train loss', 'train acc', 'test acc'])
-    for epoch in range(num_epochs):
-        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
-        test_acc = evaluate_accuracy(net, test_iter)
-        animator.add(epoch + 1, train_metrics + (test_acc,))
-    train_loss, train_acc = train_metrics
-    assert train_loss < 0.5, train_loss
-    assert train_acc <= 1 and train_acc > 0.7, train_acc
-    assert test_acc <= 1 and test_acc > 0.7, test_acc
-"""
 
 
